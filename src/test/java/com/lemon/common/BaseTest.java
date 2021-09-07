16b16caecd2fd4138ec230f0fe1502b28c5219f3
王俊茜 <3214941140@qq.com>
2021-09-07 18:07:43 +0800

    连接数据库

--- src/test/java/com/lemon/common/BaseTest.java
+++ src/test/java/com/lemon/common/BaseTest.java
@@ -72,25 +72,26 @@ public class BaseTest {
      * @param res 实际响应结果
      */
     public void assertResponse(CasePojo cp,Response res){
-
         //断言
         // 从表格数据里面取出来期望的响应结果（json格式，键名：Gpath表达式，键值：对应的期望结果）
         String expected = cp.getExpected();
-        // 转成map
-        Map<String,Object> expectedMap = JSONObject.parseObject(expected);
-        // 遍历map，进行断言
-        Set<String> allKeySet = expectedMap.keySet();
-        for (String key : allKeySet) {
-            // key：我们对象的Gpath表达式
-            // 获取实际的响应结果
-            Object actualResult = res.jsonPath().get(key);
-            System.out.println("实际响应结果：" + actualResult);
-            System.out.println("实际响应结果类型：" + actualResult.getClass());
-            // 获取期望结果
-            Object expectedResult = expectedMap.get(key);
-            System.out.println("期望结果：" + expectedResult);
-            System.out.println("期望结果类型：" + expectedResult.getClass());
-            Assert.assertEquals(actualResult, expectedResult);
+        if (expected != null) {
+            // 转成map
+            Map<String, Object> expectedMap = JSONObject.parseObject(expected);
+            // 遍历map，进行断言
+            Set<String> allKeySet = expectedMap.keySet();
+            for (String key : allKeySet) {
+                // key：我们对象的Gpath表达式
+                // 获取实际的响应结果
+                Object actualResult = res.jsonPath().get(key);
+                System.out.println("实际响应结果：" + actualResult);
+                System.out.println("实际响应结果类型：" + actualResult.getClass());
+                // 获取期望结果
+                Object expectedResult = expectedMap.get(key);
+                System.out.println("期望结果：" + expectedResult);
+                System.out.println("期望结果类型：" + expectedResult.getClass());
+                Assert.assertEquals(actualResult, expectedResult);
+            }
         }
     }
 
@@ -101,16 +102,18 @@ public class BaseTest {
      */
     public void extractToEnvironment(Response res , CasePojo cp){
         String extractStr = cp.getExtractExper();
-        // 把提取表达式转成Map
-        Map<String , Object> map = JSONObject.parseObject(extractStr);
-        Set<String> allKeys = map.keySet();
-        for (String key : allKeys) {
-            // key：变量名，value:为提取的Gpath表达式
-            Object value = map.get(key);
-            // value为Object类型，jsonPath().get()方法里填的是String类型，所以必须向下转型
-            Object actualValue = res.jsonPath().get((String) value);
-            //将对应的键和值保存到环境变量中
-            Environment.envMap.put(key,actualValue);
+        if (extractStr != null) {
+            // 把提取表达式转成Map
+            Map<String , Object> map = JSONObject.parseObject(extractStr);
+            Set<String> allKeys = map.keySet();
+            for (String key : allKeys) {
+                // key：变量名，value:为提取的Gpath表达式
+                Object value = map.get(key);
+                // value为Object类型，jsonPath().get()方法里填的是String类型，所以必须向下转型
+                Object actualValue = res.jsonPath().get((String) value);
+                //将对应的键和值保存到环境变量中
+                Environment.envMap.put(key,actualValue);
+            }
         }
     }
 
